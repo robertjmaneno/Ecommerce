76a9e1e720ae186e031be7672a2bd3760b1dfb6e
Robert Maneno <rmaneno@mra.mw>
2024-03-08 21:24:44 +0200

    Created the category model

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
