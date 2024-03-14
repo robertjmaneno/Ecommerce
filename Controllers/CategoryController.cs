79c6f87b56e3c4227be1f46a8c4c82e2bca0279c
Robert Maneno <robertjmaneno@gmail.com>
2024-03-14 13:57:30 +0200

    Corrected the logic to edit the category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -43,29 +43,43 @@ namespace Ecommerce.Controllers
             return View();
 		}
 
+
+
+
         [HttpGet]
-        public IActionResult Edit()
-        {
-            return View();
-        }
-
-
-
-        [HttpPost]
         public IActionResult Edit(int? id)
         {
-            if(id ==null && id==0) { 
-                return NotFound();
-            }
-
-            Category categoryFromDatabase = _db.Categories.FirstOrDefault(u=>u.CategoryId==id);
-            if(categoryFromDatabase == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            
+            Category? categoryFromDatabase = _db.Categories.FirstOrDefault(u=>u.CategoryId==id);
+            if (categoryFromDatabase == null)
+            {
+                return NotFound();
+            }
+
             return View(categoryFromDatabase);
         }
-    }
+
+        [HttpPost]
+		public IActionResult Edit(int id, Category category)
+		{
+			if (id != category.CategoryId)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(category);
+				_db.SaveChanges();
+				return RedirectToAction("Index", "Category");
+			}
+			return View(category);
+		}
+
+
+	}
 }
